adc752a53c0682554acd49c0c325af8165dc1f04
unmilb <unmilbind@gmail.com>
2024-12-16 17:13:07 +0530

    Found backend issue

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,5 +1,5 @@
 module "eks" {
-  source             = "./modules/eks"
+  source             = "../../modules/eks"
   cluster_name       = var.cluster_name
   vpc_cidr           = var.vpc_cidr
   subnet_cidrs       = var.subnet_cidrs
